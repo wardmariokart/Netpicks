8189db1b456d3ce965e5fc0e0b8dfb6f15ce4fe1
wilma-V <wilma.vandemaele@student.howest.be>
2020-12-05 09:47:41 +0100

    changes layout.php

--- src/view/layout.php
+++ src/view/layout.php
@@ -5,7 +5,6 @@
   <meta charset="UTF-8">
   <meta name="viewport" content="width=device-width, initial-scale=1.0">
   <title><?php echo $title; ?></title>
-  <?php /* NEW */ ?>
   <?php echo $css; ?>
   <link rel="stylesheet" href="https://use.typekit.net/sha2dsc.css">
   <link rel="stylesheet" href="https://use.typekit.net/sha2dsc.css">
