c171cc62c1b8d0cae8b0ff447b6c547fd65b1cdc
ward <ward.van.bever@student.howest.be>
2020-12-09 12:49:59 +0100

    limit amount of actors shown on detail page

--- src/dao/ImdbActorsDAO.php
+++ src/dao/ImdbActorsDAO.php
@@ -7,11 +7,12 @@ class ImdbActorsDAO extends DAO {
     parent::__construct('imdb_characters');
   }
 
-  public function selectActorsByMovieId($movieId)
+  public function selectActorsByMovieId($movieId , $limit = 5)
   {
-    $sql = "SELECT `imdb_actors`.* from `imdb_characters` INNER JOIN `imdb_actors` ON `imdb_characters`.`actor_id` = `imdb_actors`.`id` WHERE `imdb_characters`.`movie_id` = :movieId";
+    $sql = "SELECT `imdb_actors`.* from `imdb_characters` INNER JOIN `imdb_actors` ON `imdb_characters`.`actor_id` = `imdb_actors`.`id` WHERE `imdb_characters`.`movie_id` = :movieId LIMIT :limit";
     $stmt = $this->pdo->prepare($sql);
     $stmt->bindValue(':movieId', $movieId);
+    $stmt->bindValue(':limit', $limit);
     $stmt->execute();
     return $stmt->fetchAll(PDO::FETCH_ASSOC);
   }
